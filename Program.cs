c2e44572e12187be83b43d4d5cd8b20d7478e6c8
chida <48751549+cpalani1@users.noreply.github.com>
2019-03-20 13:48:37 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApplications
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my First Programe");
+            Console.WriteLine("Enter you're name :");
+            String SomeDataFromusers = Console.ReadLine();
+            try
+            {
+                if (SomeDataFromusers.Length == 0)
+                {
+                    throw new Exception("Name should be blank!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                //Console.ReadLine();
+                return;
+            }
+            Console.WriteLine("Enter you're age:");
+            int Age = 0;
+            try
+            {
+                if (int.TryParse(Console.ReadLine(),out Age)) {
+                    Console.WriteLine("You're name is " + SomeDataFromusers + " and age is " + Age);
+                    Console.ReadLine();
+                }
+                else { 
+                 throw new Exception("You're Age should be numberic");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+               // Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
